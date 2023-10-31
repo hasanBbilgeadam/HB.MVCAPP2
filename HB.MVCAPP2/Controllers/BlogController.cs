adbea001a981ec1a6017da326d7e4010dcde8e3b
hasan.baysal <hasan.baysal@ANK3-YZLMORT-08>
2023-10-31 15:39:09 +0300

    data added!

--- HB.MVCAPP2/Controllers/BlogController.cs
+++ HB.MVCAPP2/Controllers/BlogController.cs
@@ -8,32 +8,50 @@ namespace HB.MVCAPP2.Controllers
 
         public static List<Blog> blogList = new()
         {
-
-            new(){
-                Id = 1,
-                Content="example blog content , example blog content, example blog content",
-                Header="blog header",
-                ImagePath="https://picsum.photos/id/1/200/300"
-            },
-             new(){
+               new(){
                 Id = 2,
                 Content="example blog content , example blog content, example blog content",
                 Header="blog header",
-                ImagePath="https://picsum.photos/id/1/200/300"
-            },
+                ImagePath="https://picsum.photos/id/1/200/300",
 
+
+                     Comments = new()
+                {
+
+                      new(){ Id = 105,CommentContent="comment xx",BlogId=2,CommentDate = DateTime.Now.AddDays(-1)},
+                    new(){ Id = 106,CommentContent="comment xx",BlogId=2,CommentDate = DateTime.Now.AddHours(-1)},
+                    new(){ Id = 107,CommentContent="comment xx",BlogId=2,CommentDate = DateTime.Now.AddHours(-222)},
+
+                },
+            },
                new(){
                 Id = 3,
                 Content="example blog content , example blog content, example blog content",
                 Header="blog header",
-                ImagePath="https://picsum.photos/id/1/200/300"
+                ImagePath="https://picsum.photos/id/1/200/300",
+                    Comments = new()
+                {
+
+                      new(){ Id = 201,CommentContent="comment xx",BlogId=3,CommentDate = DateTime.Now.AddDays(-1)},
+                    new(){ Id = 202,CommentContent="comment xx",BlogId=3,CommentDate = DateTime.Now.AddHours(-101)},
+                    new(){ Id = 203,CommentContent="comment xx",BlogId=3,CommentDate = DateTime.Now.AddHours(-1)},
+
+                },
             },
                new(){
                 Id = 4,
                 Content="example blog content , example blog content, example blog content",
                 Header="blog header",
-                ImagePath="https://picsum.photos/id/1/200/300"
-            },
+                ImagePath="https://picsum.photos/id/1/200/300",
+                    Comments = new()
+                {
+
+                      new(){ Id = 500,CommentContent="comment xx",BlogId=4,CommentDate = DateTime.Now.AddDays(-1)},
+                    new(){ Id = 501,CommentContent="comment xx",BlogId=4,CommentDate = DateTime.Now.AddMinutes(-10)},
+                    new(){ Id = 503,CommentContent="comment xx",BlogId=4,CommentDate = DateTime.Now.AddHours(-222)},
+
+                },
+            }
 
         };
         public IActionResult Index()
